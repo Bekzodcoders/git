ac5774bb3de66be120fbafb9a53f48a4bdcbb6d5
bekzod <bekzodshodiyev999@gmail.com>
2024-02-02 07:01:53 -0600

    ds

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("salom");
         while (true){
-
+            int x = 10+20;
+            System.out.println(x);
         }
     }
 }
